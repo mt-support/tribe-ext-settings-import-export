97056768c71db5d85940a0de82716cb8b4a0efb8
András Guseo <andras@divi-magazine.com>
2020-05-29 23:44:08 +0200

    Fixing the file import

--- tribe-ext-settings-import-export.php
+++ tribe-ext-settings-import-export.php
@@ -263,8 +263,8 @@ if (
 			 */
 			if ( ! empty ( $_POST['import'] ) ) {
 
-				$import_file     = Tribe__Utils__Array::get( $_FILES[ 'import_file' ][ 'tmp_name' ], false );
-				$import_filename = Tribe__Utils__Array::get( $_FILES[ 'import_file' ][ 'name' ], false );
+				$import_file     = Tribe__Utils__Array::get( $_FILES, [ 'import_file', 'tmp_name' ], false );
+				$import_filename = Tribe__Utils__Array::get( $_FILES, [ 'import_file', 'name' ], false );
 
 				if ( empty( $import_file ) ) {
 					wp_die( __( 'Please upload a file to import.', 'tribe-ext-settings-import-export' ), __( 'Import error', 'tribe-ext-settings-import-export' ), [ 'back_link' => true ] );
